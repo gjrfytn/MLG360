edd30e39cd18f85395a5c201171a36f25e140bf6
Dmitry <dimon13142@mail.ru>
2019-12-11 00:23:02 +0300

    Precision fix

--- Strategy/Rectangle.cs
+++ Strategy/Rectangle.cs
@@ -4,11 +4,13 @@ namespace MLG360.Strategy
 {
     internal abstract class Rectangle
     {
+        private const float Epsilon = 0.000001f;
+
         private float HalfWidth => Width / 2;
         private float HalfHeight => Height / 2;
-        private Vector2 TopLeft => Center + new Vector2(-HalfWidth, HalfHeight);
-        private Vector2 TopRight => Center + new Vector2(HalfWidth, HalfHeight);
-        private Vector2 BottomRight => Center + new Vector2(HalfWidth, -HalfHeight);
+        private Vector2 TopLeft => Center + new Vector2(-HalfWidth, HalfHeight - Epsilon);
+        private Vector2 TopRight => Center + new Vector2(HalfWidth - Epsilon, HalfHeight - Epsilon);
+        private Vector2 BottomRight => Center + new Vector2(HalfWidth - Epsilon, -HalfHeight);
         private Vector2 BottomLeft => Center - new Vector2(HalfWidth, HalfHeight);
 
         protected abstract Vector2 Center { get; }
